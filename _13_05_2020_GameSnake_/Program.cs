139aecfd78d8641028d2f7492fa986ec8cfb7f31
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-05-13 23:12:29 +0300

    Добавьте файлы проекта.

---
+++ _13_05_2020_GameSnake_/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_05_2020_GameSnake_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
